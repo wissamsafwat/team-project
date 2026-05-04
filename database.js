9e9ccaccc38336644d6d6c885815dfb699d8be4c
wissamsafwat <wissamsafwat@gmail.com>
2026-05-04 13:19:28 +0300

    Add files via upload

--- database.js
+++ database.js
@@ -5,6 +5,7 @@ const gamesDatabase = [
         price: "39.99",
         rating: "9 / 10",
         req: "Core i5, 8GB RAM, GTX 1060",
+        req_rec: "Core i7, 16GB RAM, RTX 2060", 
         storage: "75 GB",
         id: "spider_man",
         info: "Spider-Man is an action-adventure video game developed by Insomniac Games and published by Sony Interactive Entertainment. It is based on the Marvel Comics character Spider-Man and was released in 2022."
@@ -15,6 +16,7 @@ const gamesDatabase = [
         price: "29.99",
         rating: "8.5 / 10",
         req: "Core i5, 12GB RAM, GTX 1650",
+        req_rec: "Core i7, 16GB RAM, RTX 3060",
         storage: "125 GB",
         id: "cod",
         info: "Call of Duty is a first-person shooter video game franchise developed by Infinity Ward and published by Activision. The series is known for its realistic portrayal of warfare and intense combat scenarios."
@@ -25,6 +27,7 @@ const gamesDatabase = [
         price: "59.99",
         rating: "8 / 10",
         req: "Core i7, 16GB RAM, RTX 2060",
+        req_rec: "Core i7, 16GB RAM, RTX 3070",
         storage: "100 GB",
         id: "fifa",
         info: "EA SPORTS FC is a football simulation video game developed by EA Sports. It is the successor to FIFA and was first released in 2023."
@@ -35,6 +38,7 @@ const gamesDatabase = [
         price: "49.99",
         rating: "9.2 / 10",
         req: "Core i5, 8GB RAM, GTX 1070",
+        req_rec: "Core i7, 16GB RAM, RTX 3060",
         storage: "110 GB",
         id: "forza",
         info: "Forza Horizon 5 is a racing video game developed by Playground Games and published by Microsoft Studios. It is the fifth installment in the Forza Horizon series and was released in 2021."
@@ -45,6 +49,7 @@ const gamesDatabase = [
         price: "39.99",
         rating: "9.8 / 10",
         req: "Core i7, 16GB RAM, RTX 2070",
+        req_rec: "Core i9, 16GB RAM, RTX 3080",
         storage: "90 GB",
         id: "god_of_war",
         info: "God of War is an action-adventure video game developed by Santa Monica Studio and published by Sony Interactive Entertainment. It is the sequel to the 2018 game God of War and was released in 2022."
@@ -55,6 +60,7 @@ const gamesDatabase = [
         price: "59.99",
         rating: "10 / 10",
         req: "Core i9, 32GB RAM, RTX 3080",
+        req_rec: "Core i9, 64GB RAM, RTX 4090",
         storage: "150 GB",
         id: "gta",
         info: "Grand Theft Auto VI is an upcoming action-adventure video game developed by Rockstar Games. It is the sixth installment in the Grand Theft Auto series and is scheduled for release in 2023."
@@ -65,6 +71,7 @@ const gamesDatabase = [
         price: "14.99",
         rating: "9.5 / 10",
         req: "Core i3, 4GB RAM, Integrated GPU",
+        req_rec: "Core i5, 8GB RAM, GTX 1050",
         storage: "2 GB",
         id: "minecraft",
         info: "Minecraft is a sandbox video game developed by Mojang Studios. It was created by Markus Persson and released in 2011. The game allows players to build and explore virtual worlds made up of blocks."
@@ -75,6 +82,7 @@ const gamesDatabase = [
         price: "29.99",
         rating: "8.2 / 10",
         req: "Core i5, 8GB RAM, GTX 1060",
+        req_rec: "Core i7, 16GB RAM, RTX 2060",
         storage: "50 GB",
         id: "nfs",
         info: "Need for Speed is a racing video game franchise developed by EA Games. The series is known for its high-speed racing gameplay and diverse range of vehicles."
@@ -85,6 +93,7 @@ const gamesDatabase = [
         price: "19.99",
         rating: "8.7 / 10",
         req: "Core i3, 4GB RAM, GTX 660",
+        req_rec: "Core i5, 8GB RAM, GTX 1060",
         storage: "20 GB",
         id: "rocket_league",
         info: "Rocket League is a vehicular football video game developed and published by Psyonix. It was released for PlayStation 4 and Windows in 2015, and for Xbox One in 2016."
